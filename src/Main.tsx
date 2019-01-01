2f21ca900b748e3c1754254509f576fd3408fafb
Ashley Ng <ashleyng@users.noreply.github.com>
2019-01-01 15:15:52 -0600

    hide status bar

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { Button } from './components/common';
 
 export default class MainClass extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>Hello World</Text>
+        <StatusBar hidden={true} />
         <Button>
             Button1
         </Button>
